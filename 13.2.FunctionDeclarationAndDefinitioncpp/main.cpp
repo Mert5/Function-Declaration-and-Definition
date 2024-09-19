f8f6f0a0cd4b0043204c3a847e0e1e526320d22c
Mert5 <39389585+Mert5@users.noreply.github.com>
2024-09-19 14:41:04 +0300

    Add files via upload

---
+++ 13.2.FunctionDeclarationAndDefinitioncpp/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+// REFERENCE => freeCodeCamp.org
+
+
+int max(int x, int y);  // Function declaration, prototype
+
+int min(int x, int y);  // variable names aren't really important in function declaration side.
+
+int inc_mult(int , int );   // you can even leave variable names empty in declaration
+
+int main(){
+
+    int m{66};
+    int n{56};
+    int compare = max(m,n);
+    int little = min(m,n);
+
+    std::cout << "Maximum is : " << compare << std::endl;
+    
+    std::cout << "Minimum is : " << little << std::endl;
+
+    std::cout << "Multiplication of " << m+1 << " and " << n+1 << " is : " << inc_mult(m,n) << std::endl;
+
+    return 0;
+}
+
+int max(int x, int y){  // Function definition - shows up after main
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+
+int min(int x, int y){  // variable names are important in function definition side.
+    if(x>y)
+        return y;
+    else
+        return x;
+}
+
+int inc_mult(int x, int y){
+
+    int result = (++x) * (++y);
+
+    return result;      // or just =>  return (++x) * (++y);
+
+}
